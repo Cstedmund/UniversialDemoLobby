cc27e598be91e268f34b68eb1c0750543c4627e1
Edmund.T <92370517+Cstedmund@users.noreply.github.com>
2022-10-01 01:33:26 +0800

    Update ULobbyController.cs

--- Assets/UniversialDemoLobby/Script/ULobbyController.cs
+++ Assets/UniversialDemoLobby/Script/ULobbyController.cs
@@ -10,28 +10,18 @@ namespace UniversialLobby {
     public class ULobbyController : MonoBehaviour {
         [SerializeField]
         private GameObject m_ButtonHolder, m_ButtonToSpawn;
-        private ULobbyManager _uLobbyManager;
+        //private ULobbyManager _uLobbyManager;
 
         void Start() {
-            _uLobbyManager = ULobbyManager.Instance;
+            //_uLobbyManager = ULobbyManager.Instance;
             var sceneCount = SceneManager.sceneCountInBuildSettings;
-
             for (var i = 1; i < sceneCount; i++) {
                 var tempBrn = Instantiate(m_ButtonToSpawn, m_ButtonHolder.transform);
-                tempBrn.transform.GetChild(0).GetComponent<Text>().text = GetSceneNameByBuildIndex(i);
-                //tempBrn.GetComponent<Button>().onClick.AddListener(() => { var _ = StartCoroutine(LoadSceneAsync(idx)); });
-                tempBrn.GetComponent<Button>().onClick.AddListener((UnityEngine.Events.UnityAction)delegate { SelectScenenLoad(tempBrn);});
-                //var trigger = tempBrn.GetComponent<EventTrigger>();
-                //var entry = new EventTrigger.Entry();
-                //entry.eventID = EventTriggerType.PointerUp;
-                //entry.callback.AddListener(data => { SetCurrentBook(_bookItem); });
-                //trigger.triggers.Add(entry);
+                var buildIndex = i;
+                tempBrn.transform.GetChild(0).GetComponent<Text>().text = GetSceneNameByBuildIndex(buildIndex);
+                tempBrn.GetComponent<Button>().onClick.AddListener(() => { var _ = StartCoroutine(LoadSceneAsync(buildIndex)); });
             }
             m_ButtonHolder.transform.GetChild(0).GetComponent<Button>().interactable = false;
-        }
-
-        private void SelectScenenLoad(GameObject obj) {
-            _uLobbyManager.InternalLoadScene(obj.transform.GetChild(0).GetComponent<Text>().text);
         }
 
         private string GetSceneNameByBuildIndex(int buildIndex) {
@@ -39,7 +29,7 @@ namespace UniversialLobby {
             return Path.GetFileNameWithoutExtension(path);
         }
 
-        public IEnumerator LoadSceneAsync(int buildIndex) {
+        private IEnumerator LoadSceneAsync(int buildIndex) {
             Debug.Log(buildIndex);
             var sceneLoadReq = SceneManager.LoadSceneAsync(buildIndex);
             yield return new WaitUntil(() => sceneLoadReq.isDone);
